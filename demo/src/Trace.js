b364a0c0677b3b7d16d675f3c6981da37d31ff7b
James Friend <james@jsdf.co>
2018-10-17 01:31:38 -0700

    canvas renderer improvements

--- demo/src/Trace.js
+++ demo/src/Trace.js
@@ -2,6 +2,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import memoize from 'memoize-one';
+import Flatbush from 'flatbush';
 import transformTrace from './calculateTraceLayout';
 import type {RenderableMeasure} from './calculateTraceLayout';
 
@@ -13,6 +14,9 @@ type Measure = {
 
 const BAR_HEIGHT = 20;
 const PX_PER_MS = 1;
+const DRAW_LIMIT = 500;
+const DRAW_MIN_PERCENT = 0.3;
+const DRAW_TEXT_MIN_PERCENT = 1;
 
 function memoizeWeak<TArg, TRet>(fn: (arg: TArg) => TRet): (arg: TArg) => TRet {
   const cache = new WeakMap();
@@ -46,7 +50,7 @@ type State = {
   selection: ?RenderableMeasure<Measure>,
 };
 
-// const run = fn => requestIdleCallback(fn);
+// const run = fn => requestAnimationFrame(fn);
 const run = fn => fn();
 
 function loadValue(name: string, defaultVal: number) {
@@ -68,10 +72,14 @@ export default class Trace extends React.Component<Props, State> {
   constructor(props: Props) {
     super(props);
     const {startOffset, size} = this._getExtents();
+    const zoom = loadValue('zoom', 1);
     this.state = {
       selection: null,
-      center: loadValue('center', startOffset + size / 2),
-      zoom: loadValue('zoom', 1),
+      center: loadValue(
+        'center',
+        startOffset + this.props.viewportWidth / PX_PER_MS / 2
+      ),
+      zoom,
     };
   }
 
@@ -82,7 +90,18 @@ export default class Trace extends React.Component<Props, State> {
     };
   }
 
-  _transformTrace = memoize(trace => transformTrace(trace).slice(0, 1000));
+  _transformTrace = memoize(trace => transformTrace(trace));
+
+  _getFlatbush = memoize(renderableTrace => {
+    const index = new Flatbush(renderableTrace.length);
+    for (const p of renderableTrace) {
+      index.add(p.startTime, 0, p.startTime + p.duration, 1);
+    }
+    // perform the indexing
+    index.finish();
+
+    return index;
+  });
 
   _handleZoom = (event: SyntheticMouseEvent<HTMLInputElement>) => {
     const updated = parseFloat(event.currentTarget.value);
@@ -100,7 +119,7 @@ export default class Trace extends React.Component<Props, State> {
 
   _handleLeft = (event: SyntheticMouseEvent<HTMLInputElement>) => {
     const {size} = this._getExtents();
-    const updated = this.state.center - size * 0.05 / this.state.zoom;
+    const updated = this.state.center - size * 0.01 / this.state.zoom;
     run(() => {
       this.setState({center: updated});
     });
@@ -108,7 +127,7 @@ export default class Trace extends React.Component<Props, State> {
 
   _handleRight = (event: SyntheticMouseEvent<HTMLInputElement>) => {
     const {size} = this._getExtents();
-    const updated = this.state.center + size * 0.05 / this.state.zoom;
+    const updated = this.state.center + size * 0.01 / this.state.zoom;
     run(() => {
       this.setState({center: updated});
     });
@@ -119,6 +138,35 @@ export default class Trace extends React.Component<Props, State> {
       selection: this._transformTrace(this.props.trace)[
         parseInt(event.currentTarget.getAttribute('data-index'))
       ],
+    });
+  };
+
+  _dragging = false;
+
+  _dragStart = (event: SyntheticMouseEvent<HTMLCanvasElement>) => {
+    this._dragging = true;
+  };
+  _dragMove = (event: SyntheticMouseEvent<HTMLCanvasElement>) => {
+    if (this._dragging) {
+      const updated =
+        this.state.center - event.movementX / PX_PER_MS / this.state.zoom;
+      run(() => {
+        this.setState({center: updated});
+      });
+    }
+  };
+  _dragEnd = (event: SyntheticMouseEvent<HTMLCanvasElement>) => {
+    this._dragging = false;
+  };
+
+  _handleWheel = (event: SyntheticMouseEvent<HTMLCanvasElement>) => {
+    // todo: zoom centered on mouse
+    // const rect = event.currentTarget.getBoundingClientRect();
+    // const x = event.clientX - rect.left;
+    // const updatedCenter = this.state.center;
+    const updatedZoom = this.state.zoom * (1 + 0.01 * -event.deltaY);
+    run(() => {
+      this.setState({zoom: updatedZoom});
     });
   };
 
@@ -162,13 +210,12 @@ export default class Trace extends React.Component<Props, State> {
     }
   };
 
+  _handleScroll() {}
+
   componentDidMount() {
     document.addEventListener('keypress', this._handleKey);
     if (this.props.renderer == 'canvas') {
       const canvas = this._canvas;
-      if (canvas instanceof HTMLCanvasElement && canvas.parentElement) {
-        canvas.width = canvas.parentElement.getBoundingClientRect().width;
-      }
       this._renderCanvas();
     }
   }
@@ -196,22 +243,20 @@ export default class Trace extends React.Component<Props, State> {
     };
   }
 
-  _getLayout(
-    measure: RenderableMeasure<Measure>,
-    extents: {startOffset: number, endOffset: number}
-  ) {
-    const {startOffset, endOffset} = extents;
+  _getContentWidth() {
+    const {size} = this._getExtents();
+    return size * PX_PER_MS * this.state.zoom;
+  }
 
+  _getLayout(measure: RenderableMeasure<Measure>) {
     const centerOffset = this.state.center;
 
-    const width = measure.measure.duration * PX_PER_MS * this.state.zoom,
-      height = BAR_HEIGHT,
-      x =
-        (measure.measure.startTime - centerOffset) *
-          PX_PER_MS *
-          this.state.zoom +
-        this.props.viewportWidth / 2,
-      y = measure.stackIndex * BAR_HEIGHT;
+    const width = measure.measure.duration * PX_PER_MS * this.state.zoom;
+    const height = BAR_HEIGHT;
+    const x =
+      (measure.measure.startTime - centerOffset) * PX_PER_MS * this.state.zoom +
+      this.props.viewportWidth / 2;
+    const y = measure.stackIndex * BAR_HEIGHT;
 
     return {
       width,
@@ -222,10 +267,30 @@ export default class Trace extends React.Component<Props, State> {
     };
   }
 
+  _getCanvasContext = memoize((canvas: HTMLCanvasElement) => {
+    // hidpi canvas: https://www.html5rocks.com/en/tutorials/canvas/hidpi/
+
+    // Get the device pixel ratio, falling back to 1.
+    var dpr = window.devicePixelRatio || 1;
+    // Get the size of the canvas in CSS pixels.
+    var rect = canvas.getBoundingClientRect();
+    // Give the canvas pixel dimensions of their CSS
+    // size * the device pixel ratio.
+    canvas.width = rect.width * dpr;
+    canvas.height = rect.height * dpr;
+    canvas.style.width = `${rect.width}px`;
+    canvas.style.height = `${rect.height}px`;
+    var ctx = canvas.getContext('2d');
+    // Scale all drawing operations by the dpr, so you
+    // don't have to worry about the difference.
+    ctx.scale(dpr, dpr);
+    return ctx;
+  });
+
   _renderCanvas() {
     const canvas = this._canvas;
     if (canvas instanceof HTMLCanvasElement) {
-      var ctx = canvas.getContext('2d');
+      var ctx = this._getCanvasContext(canvas);
       ctx.clearRect(0, 0, canvas.width, canvas.height);
 
       const renderableTrace = this._transformTrace(this.props.trace);
@@ -235,13 +300,25 @@ export default class Trace extends React.Component<Props, State> {
 
       const extents = this._getExtents();
 
+      let drawn = 0;
+
       for (var index = 0; index < renderableTrace.length; index++) {
         const measure = renderableTrace[index];
 
-        const {width, height, x, y} = this._getLayout(measure, extents);
+        const {width, height, x, y, inView} = this._getLayout(measure);
+
+        if (!inView) {
+          continue;
+        }
 
         ctx.fillStyle = this._getMeasureColor(measure.measure);
         ctx.fillRect(x, y, width, height);
+
+        // skip text rendering for small measures
+        // text is by far the most expensive part of rendering the trace
+        if (width < this.props.viewportWidth * (DRAW_TEXT_MIN_PERCENT / 100)) {
+          continue;
+        }
 
         ctx.font = '10px Lucida Grande';
         ctx.fillStyle = 'black';
@@ -256,6 +333,9 @@ export default class Trace extends React.Component<Props, State> {
     if (renderableTrace[0] == null) {
       return <div>empty trace</div>;
     }
+
+    // number of measures drawn, used to cap render complexity
+    let drawn = 0;
 
     const extents = this._getExtents();
     const {startOffset, endOffset} = extents;
@@ -279,67 +359,87 @@ export default class Trace extends React.Component<Props, State> {
           step={String((endOffset - startOffset) * 0.0001)}
           min={String(startOffset)}
           max={String(endOffset)}
+          style={{width: 300}}
           onChange={this._handleCenter}
         />
         <button onClick={this._handleLeft}>-</button>
         <button onClick={this._handleRight}>+</button>
-
-        <div
-          style={{
-            width: this.props.viewportWidth,
-            overflowX: 'scroll',
-            height: this.props.viewportHeight,
-          }}
-        >
+        <div>
           {this.props.renderer == 'canvas' ? (
             <canvas
               ref={this._onCanvas}
+              onWheel={this._handleWheel}
+              onMouseDown={this._dragStart}
+              onMouseMove={this._dragMove}
+              onMouseUp={this._dragEnd}
               width={this.props.viewportWidth}
               height={this.props.viewportHeight}
             />
           ) : (
             <div
               style={{
-                position: 'relative',
-                fontSize: '10px',
-                whiteSpace: 'nowrap',
+                width: this.props.viewportWidth,
+                overflowX: 'scroll',
+                height: this.props.viewportHeight,
               }}
+              onScroll={this._handleScroll}
             >
-              {renderableTrace.map((measure, index) => {
-                const {width, height, x, y, inView} = this._getLayout(
-                  measure,
-                  extents
-                );
-                if (!inView) {
-                  return null;
-                }
-                return (
-                  <div
-                    key={index}
-                    data-index={index}
-                    title={`${(measure.measure.duration || 0).toFixed(2)}ms`}
-                    style={{
-                      position: 'absolute',
-                      width,
-                      height,
-                      overflow: 'hidden',
-                      backgroundColor: this._getMeasureColor(measure.measure),
-                      transform: `translate(${x}px, ${y}px)`,
-                      border:
-                        this.state.selection == measure
-                          ? 'solid red 1px'
-                          : null,
-                    }}
-                    onClick={this._handleMeasureClick}
-                  >
-                    &nbsp;
-                    {measure.measure.name}
-                  </div>
-                );
-              })}
+              <div
+                style={{
+                  position: 'relative',
+                  fontSize: '10px',
+                  whiteSpace: 'nowrap',
+                  width: this._getContentWidth(),
+                }}
+              >
+                {renderableTrace.map((measure, index) => {
+                  const {width, height, x, y, inView} = this._getLayout(
+                    measure
+                  );
+                  if (!inView) {
+                    return null;
+                  }
+                  if (
+                    width <
+                    this.props.viewportWidth * (DRAW_MIN_PERCENT / 100)
+                  ) {
+                    return null;
+                  }
+                  drawn++;
+                  if (drawn > DRAW_LIMIT) {
+                    return null;
+                  }
+                  return (
+                    <div
+                      key={index}
+                      data-index={index}
+                      title={`${(measure.measure.duration || 0).toFixed(2)}ms`}
+                      style={{
+                        position: 'absolute',
+                        width,
+                        height,
+                        overflow: 'hidden',
+                        backgroundColor: this._getMeasureColor(measure.measure),
+                        // transform: `translate(${x}px, ${y}px) translateZ(0)`,
+                        left: x,
+                        top: y,
+                        border:
+                          this.state.selection == measure
+                            ? 'solid red 1px'
+                            : null,
+                      }}
+                      onClick={this._handleMeasureClick}
+                    >
+                      &nbsp;
+                      {measure.measure.name}
+                    </div>
+                  );
+                })}
+              </div>
             </div>
           )}
         </div>
+        <span>drawn={drawn}</span>
         <pre>
           {this.state.selection
             ? JSON.stringify(this.state.selection.measure, null, 2)
