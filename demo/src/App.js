3dadb4b13e9e9c861c691cd40907c50e4ab03fa3
James Friend <james@jsdf.co>
2018-10-17 01:31:29 -0700

    add demo

---
+++ demo/src/App.js
@@ -0,0 +1,26 @@
+import React, {Component, unstable_AsyncMode as AsyncMode} from 'react';
+import logo from './logo.svg';
+import './App.css';
+import tracedata from './exampledata';
+import Trace from './Trace';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <AsyncMode>
+          <Trace
+            trace={tracedata}
+            viewportWidth={1200}
+            viewportHeight={500}
+            renderer={
+              window.location.search.slice(1) === 'dom' ? 'dom' : 'canvas'
+            }
+          />
+        </AsyncMode>
+      </div>
+    );
+  }
+}
+
+export default App;
